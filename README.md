566c9bd7afbfa03f385b51d2a24d4466d3b21d64
Yiyoung <yiyoung.md@gmail.com>
2025-09-22 17:17:54 -0400

    upload project

---
+++ README.md
@@ -0,0 +1,6 @@
+# PRT data
+PRT data in a sql database for ease of data analytics
+## How to use
+rename `.env.example` to `.env` and edit the fields
+run `python3 csvtopostgres.py` after installing dependencies to recompile postgres database from the \*.csv files in the folder
+or use compiled\_database.sql for an already compiled database
---
+++ csvtopostgres.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import psycopg2
+from dotenv import load_dotenv
+import os
+import glob
+import re
+
+# --- READ .env ---
+load_dotenv()
+
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+
+# --- Connect to PostgreSQL ---
+conn = psycopg2.connect(
+    dbname=db_name,
+    user=db_user,
+    password=db_password,
+    host=db_host,
+    port=db_port
+)
+print("Connecting to:", db_name, "as", db_user)
+cur = conn.cursor()
+
+csv_files = glob.glob("*.csv")
+for csv_path in csv_files:
+    filename = os.path.splitext(os.path.basename(csv_path))[0]
+
+    # --- Read CSV ---
+    df = pd.read_csv(csv_path)
+    df.rename({df.columns[0]: 'Department'}, inplace=True)
+
+    # --- Clean Data ---
+    # TODO: use regex to match if its a dollar symbol, then clean that data as well
+    df = df.fillna("0")
+    for i in range(2006, 2024):
+        df[str(i)] = df[str(i)].map(lambda x: str(x).replace('$', '').replace(',', '')).map(lambda x: int(x) if x.replace('-', '').isdigit() else 0)
+
+    # Convert from wide → long format
+    df_long = df.melt(id_vars=["Department"],
+                      var_name="year",
+                      value_name="value")
+
+    print("Adding", filename, "table")
+
+    # --- Create table ---
+    cur.execute(f"""
+        CREATE TABLE IF NOT EXISTS {filename} (
+            id SERIAL PRIMARY KEY,
+            department TEXT,
+            year INT,
+            value INT
+        )
+    """)
+
+    # --- Insert rows ---
+    for _, row in df_long.iterrows():
+        cur.execute(f"""
+            INSERT INTO {filename} (department, year, value)
+            VALUES (%s, %s, %s)
+        """, (row["Department"], int(row["year"]), row["value"]))
+    print("Added", filename, "to database")
+
+# Commit + close
+conn.commit()
+cur.close()
+conn.close()
